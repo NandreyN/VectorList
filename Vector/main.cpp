dd7d5259e583f600c25893520264cf4236af1f18
NandreyN <nandreyn1998@gmail.com>
2017-02-11 14:36:15 +0300

    Clear

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -2,22 +2,9 @@
 #include "CustomList.h"
 #include<algorithm>
 using namespace std;
-using CustomListComponents::CustomList;
+using namespace CustomListComponents;
 
 int main()
 {
-	CustomList<int> list;
-
-	for (int i = 0; i < 6; i++)
-	{
-		list.add(i);
-	}
-	for (int i = 0; i < 5; i++)
-		cout << list.remove() << endl;
-
-	while (list.begin() != list.end())
-	{
-		cout << list.remove() << endl;
-	}
 	return 0;
 }
